e622ecbb21dd5d43b952f70add94cfa42eb05724
satya179 <teja64062@gmail.com>
2024-12-13 12:21:23 +0530

    Create multiplicationtable.c

---
+++ multiplicationtable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,mul;
+    printf("enter n value:");
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+      mul=n*i;
+      printf("%d * %d = %d \n",n,i,mul);
+    }
+    return 0;
+}
